31a01441ea01310efd238158f65bf52c47783032
sihyeong <tlgud2325@naver.com>
2021-10-05 00:58:51 +0900

    1차 과제

---
+++ 1st-seminar/array.js
@@ -0,0 +1,25 @@
+/*
+-----------------------------
+        1. 배열 실습
+-----------------------------
+*/ 
+
+let arr1 = [];
+console.log(arr1); // [] 빈 배열 출력
+console.log(typeof arr1); // 어떤 type인지 출력
+
+let arr2 = new Array (1,2,3,4,5); //Array 생성자를 통해서 배열 생성
+console.log(arr2);
+console.log(typeof arr2);
+
+let arr3 = ["손시형",1,2,3,null,{name:"sihyeong", age: 24}];
+console.log(arr3);
+console.log(typeof arr3);
+
+/*
+---------------------------------
+    2. 배열 prototype 메서드
+---------------------------------
+*/
+
+console.log()
