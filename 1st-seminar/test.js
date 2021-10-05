dc36723d17990c541ca213de62afdade6220d4bf
sihyeong <tlgud2325@naver.com>
2021-10-06 00:41:17 +0900

    Create test.js

---
+++ 1st-seminar/test.js
@@ -0,0 +1,32 @@
+//변수 선언법
+
+//var : 재선언 / 재할당 가능
+
+/*
+var variableVar ="123";
+variableVar="321";
+
+console.log("variableVar",variableVar);
+*/
+
+//let : 재선언 불가 / 재할당가능
+
+/*
+let variableVar ="123";
+variableVar="321";
+
+console.log("variableVar",variableVar);
+*/
+
+//const : 재선언 불가/ 재할당 불가
+/*
+const variableConst ="123";
+variableConst = "321";
+
+console.log("variableConst",variableConst);
+*/
+
+var somVar;
+let somlet;
+
+const someConst;
