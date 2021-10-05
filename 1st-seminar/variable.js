cae4f92232850e8f70b57730cf677ce2c5a2d008
sihyeong <tlgud2325@naver.com>
2021-10-06 00:53:52 +0900

    Create variable.js

---
+++ 1st-seminar/variable.js
@@ -0,0 +1,51 @@
+//재선언
+
+//var 재선언 가능!
+/*
+var variableVar="123";
+var variableVar="321";
+
+console.log("variableVar",variableVar);
+*/
+
+//let 재선언 불가능!
+/*
+let variablelet ="123";
+let variablelet="321";
+
+console.log("variablelet",variablelet);
+*/
+
+//const 재선언 불가능!
+/*
+const variableConst="123";
+const variableConst="321";
+
+console.log("variableconst",variableConst);
+*/
+
+//재할당
+
+//var 재할당도 가능
+/*
+var variableVar ="123";
+variableVar="321";
+
+console.log("variableVar",variableVar);
+*/
+
+//let 재할당만 가능
+/*
+let variablelet="123";
+variablelet="321";
+
+console.log("variablelet",variablelet);
+*/
+
+//const 재할당도 불가능
+/*
+const variableConst="123";
+variableConst="321";
+
+console.log("variableConst",variableConst);
+*/
