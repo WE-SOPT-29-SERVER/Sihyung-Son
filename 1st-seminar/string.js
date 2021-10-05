b4e6d12424c36e5e16ce24c24964c934ff0e1917
sihyeong <tlgud2325@naver.com>
2021-10-06 00:07:59 +0900

    Create string.js

---
+++ 1st-seminar/string.js
@@ -0,0 +1,13 @@
+const str1 ="Just do it";
+const str2 ="Just don't do it";
+
+("");
+("");
+
+const myName = "손시형";
+
+const str ="안녕하세요"+ myName +"입니다.";
+const backticStr =`안녕하세요 ${myName}입니다`;
+
+console.log(str);
+console.log(backticStr);
