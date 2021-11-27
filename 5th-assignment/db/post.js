9111b2c5f28dcf9d5661a6e0520fedbb8bb8f611
sihyeong <tlgud2325@naver.com>
2021-11-27 14:10:08 +0900

    [UPDATE] post

--- 5th-assignment/db/post.js
+++ 5th-assignment/db/post.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 const getPostByUserId = async(client, userId)=>{
-    const{ rows } = await client.qurey(
+    const{ rows } = await client.query(
         `
         SELECT * FROM post
         WHERE user_id = $1
@@ -15,10 +15,10 @@ const getPostByUserId = async(client, userId)=>{
 
 const getPostByUserIds = async(client, userIds)=>{
     if(userIds.length < 1) return []
-    const{ rows } = await client.qurey(
+    const{ rows } = await client.query(
         `
         SELECT * FROM post
-        WHERE user_id IN ${userIds.join()}
+        WHERE user_id IN (${userIds.join()})
         AND is_deleted =FALSE
         `,
     );
